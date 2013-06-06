9531e7385f5fa62e143b678b975b71cb630e0991
Bruno Silva <bruno.silva@webdetails.com>
2013-06-06 19:12:40 +0100

    All dashboards: moving table buttons from postExec to new AddIn "parameterButton"

--- static/custom/js/cpkUI.js
+++ static/custom/js/cpkUI.js
@@ -27,15 +27,41 @@ return { siteMap: siteMap,
 
 /*********************************** Functions ******************************************/
 
-cpkUI.configViewPluginOp = function() {
-	var $tableBodyTrArray = $('#'+this.htmlObject).find('table tbody tr');
-	$tableBodyTrArray.each(function(){
-		var $tdArray = $(this).find('td');
-		/* */
-		var $placeHolder = $($tdArray[$tdArray.length - 1]);
-		var pluginId = $placeHolder.text();
-		$placeHolder.empty().addClass('viewPluginButtonContainer');
-		var $viewPluginButton = $("<button type='button'>View plugin</button>").addClass('button');
-		$placeHolder.append($viewPluginButton);
-	});
-}
+
+
+/************************************  AddIns ************************************/
+
+
+;(function (){
+
+	var parameterButton = {
+	    name: "parameterButton",
+	    label: "Parameter Button",
+	    defaults: {
+	    	buttonClass: "viewButton",
+	    	buttonTitle: "View",
+	      	buttonAction: function(v, st) {
+	      		Dashboards.log(v);
+	      	}
+	    },
+
+	    init: function(){
+	      	$.fn.dataTableExt.oSort[this.name+'-asc'] = $.fn.dataTableExt.oSort['string-asc'];
+	      	$.fn.dataTableExt.oSort[this.name+'-desc'] = $.fn.dataTableExt.oSort['string-desc'];
+	    },
+	    
+	    implementation: function(tgt, st, opt){
+	    	var $button = $("<button class='"+opt.buttonClass+" '>"+opt.buttonTitle+"</button>");
+	 		/* onclick='"opt.buttonAction.call(this, st)"' */
+	    	$button.click(function(){
+	    		opt.buttonAction(st.value, st);
+	    	});
+	    	var $cell = $(tgt);
+	    	$cell.empty();
+	    	$cell.append($button);
+	    }
+
+  	};
+  	Dashboards.registerAddIn("Table", "colType", new AddIn(parameterButton));
+
+})();
