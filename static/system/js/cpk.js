2d613992e553a0329ca2ad2dcd91eeaa50e23ac4
Paulo <paulo.pires@webdetails.pt>
2013-06-11 12:37:53 +0100

    edit table cells js and css

--- static/system/js/cpk.js
+++ static/system/js/cpk.js
@@ -26,9 +26,15 @@
 
     implementation: function (tgt, st, opt) {
       var t = $(tgt);
-	  selectHTML = "<textarea>" + st.value + "</textarea>";
-	  /*t.click(function(){t.append(selectHTML);});
-	  t.append(selectHTML);*/
+	  var value = st.value;
+	  selectHTML = "<textarea>" + value + "</textarea>";
+	  t.empty();
+	  t.append(selectHTML);
+	  t.keyup(function(event){
+		if (event.keyCode == 13) {
+			t.load();
+		}
+	  });
     }
   };
   Dashboards.registerAddIn("Table", "colType", new AddIn(editable));
