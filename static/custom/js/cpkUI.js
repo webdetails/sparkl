d3ab0fdc1bfb63d4c1f66590103fe11fc2d56976
Paulo <paulo.pires@webdetails.pt>
2013-06-12 15:19:12 +0100

    change css and js stuff to UI files on custom dir

--- static/custom/js/cpkUI.js
+++ static/custom/js/cpkUI.js
@@ -34,5 +34,50 @@
 
   	};
   	Dashboards.registerAddIn("Table", "colType", new AddIn(parameterButton));
+	
+	
+/* edit data of table  */
+  var editable = {
+    name: "editable",
+    label: "Editable",
+    defaults: {
+		action: function (v, st) {
+			Dashboards.log(v);
+		}
+    },
+    init: function(){
+        
+      // Register this for datatables sort
+      var myself = this;
+      $.fn.dataTableExt.oSort[this.name+'-asc'] = function(a,b){
+        return myself.sort(a,b)
+      };
+      $.fn.dataTableExt.oSort[this.name+'-desc'] = function(a,b){
+        return myself.sort(b,a)
+      };     
+    }, 
+    sort: function(a,b){
+      return this.sumStrArray(a) - this.sumStrArray(b);
+    }, 
 
+    implementation: function (tgt, st, opt) {
+      var t = $(tgt);
+	  var value = st.value;
+	  var text = $("<input/>").attr({value:value, type:'text'})
+		.keyup(function(event){
+			if (event.keyCode == 13) {
+				opt.action( $(this).val(), st );
+			}
+			/*var idx = this.parentNode.parentNode.rowIndex;
+			metadataParam[idx-1][1] = $(this).val();*/
+			var obj = this.parentNode.parentNode.children[0].textContent;
+			metadataParam[obj.toString()] = $(this).val();
+		});
+	  
+	  t.empty();
+	  t.append(text);
+    }
+  };
+  Dashboards.registerAddIn("Table", "colType", new AddIn(editable));
+	
 })();
