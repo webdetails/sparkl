1c07bc4a54b490ccdc78d9ce268b8b75346dfc63
Bruno Silva <bruno.silva@webdetails.com>
2013-07-19 16:12:41 +0100

    pluginCard CDE part JS file + XML file: 1st commit

---
+++ resources/components/SparklCards/SparklPluginCardComponent.js
@@ -0,0 +1,119 @@
+var SparklPluginCardComponent = UnmanagedComponent.extend({
+
+  	var pluginOptsButton = [
+    	{
+      		id: "deleteOption",
+      		label: "Delete"
+    	},
+    	{
+      		id: "viewOption",
+      		label: "View"
+    	}
+  	]
+
+  	var plugins = [
+    	{
+      		plugin_id: 'plugin1',
+      		plugin_description: 'A plugin made of gold',
+      		plugin_name: 'BSilva Breakfast Plugin',
+      		version: '01a13',
+      		actionOpts: pluginOptsButton,
+      		imgSrc: '../img/alpha.png'
+    	},
+    	{
+      		plugin_id: 'plugin2',
+      		plugin_description: 'Testing some stuff 22222',
+      		plugin_name: 'BSilva Lunch Plugin',
+      		version: '01a13',
+      		actionOpts: pluginOptsButton,
+      		imgSrc: '../img/beta.png'
+    	},
+    	{
+      		plugin_id: 'plugin3',
+      		plugin_description: 'Testing some stuff 3333',
+      		plugin_name: 'BSilva Tea Plugin',
+    	  	version: '01a13',
+     	 	actionOpts: pluginOptsButton,
+      		imgSrc: '../img/gamma.png'
+    	},
+    	{
+      		plugin_id: 'plugin4',
+      		plugin_description: 'Testing some stuff 3333',
+      		plugin_name: 'PMartins Plugin',
+      		version: '01a13',
+      		actionOpts: pluginOptsButton,
+      		imgSrc: '../img/omega.png'
+    	}
+  	]
+
+	update: function() {
+		$.extend(this.options,this);
+	   	this.ph = $("#" + this.htmlObject);
+	   	var redraw = _.bind(this.redraw,this);
+	   	this.synchronous(redraw);
+	},
+	redraw: function() {
+    	var cd = this.chartDefinition;
+/*    	var modelOptions = $.extend({
+        	sow: (_.isNumber(cd.startOfWeek)) ? cd.startOfWeek : 1,
+          	selection: Dashboards.getParameterValue( this.parameter ) || {}
+        }, cd );
+*/
+
+    	/* Initialize model and view, if needed */
+  		_.each( plugins , function(pluginOpts){
+
+  			if( !this.pluginCardModel ){
+    			this.pluginCardModel[pluginOpts.plugin_id] = new wd.cpk.models.sparklPluginCard( pluginOpts );
+    		} else {
+    			this.pluginCardModel[pluginOpts.plugin_id].set( pluginOpts );
+    		}
+
+    		if( !this.pluginCardView[pluginOpts.plugin_id] ){
+	    		this.pluginCardView[pluginOpts.plugin_id] = new wd.cpk.views.sparklPluginCard({	
+	      			model: pluginCardModel[pluginOpts.plugin_id],
+	      			tagName: 'div'
+	    		});
+	    	}
+    		pluginCardView.render( '#' + this.htmlObject );
+    		this.configureListeners();
+    		/*this.selectorModel.syncSelection();*/
+  		});
+  	}
+	configureListeners: function (){
+
+		this.pluginCardModel[pluginOpts.plugin_id].off('action:deleteOption');
+		this.pluginCardModel[pluginOpts.plugin_id].off('action:viewOption');
+
+    	this.pluginCardModel[pluginOpts.plugin_id].on('action:deleteOption',function(id){
+      		Console.log('Deleting '+id);
+    	},this);
+    	this.pluginCardModel[pluginOpts.plugin_id].on('action:viewOption',function(id){
+      		Console.log('Viewing '+id);
+    	},this);
+
+/*    	this.selectorModel.off('change:selection');
+    	this.selectorModel.off('fullReport');
+    	this.selectorModel.off('export');
+
+    	this.selectorModel.on('change:selection', this.handleSelectionChange, this);
+    	this.selectorModel.on('export', function (){
+      		var comp = Dashboards.getComponentByName( this.chartDefinition.exportComponent ),
+          		q = comp.query || comp.queryState;
+      		if (q){
+        		q.exportData('xls');
+      		} else {
+        		Dashboards.log('No query object found in component ' + comp.name );
+      		}
+    	}, this);
+    	this.selectorModel.on('fullReport' , function (){
+      		// Do nothing
+      		Dashboards.log('Not implemented yet. Override.')
+    	}, this);
+*/
+/*	},
+  	handleSelectionChange: function(evt){
+     	Dashboards.processChange(this.name);
+  	},  	
+*/
+}); 
