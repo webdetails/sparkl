b241cb99793f3ef3d19eafa528a2f386c90328fd
Goncalo Neto <goncalo.neto@webdetails.pt>
2013-08-26 15:13:54 +0100

    Add function that extends a ph with an upload function and returns a trigger for the upload.

--- static/custom/js/sparkl.js
+++ static/custom/js/sparkl.js
@@ -1,73 +1,150 @@
 /********************************** Project namespace *******************************************/
 var sparkl = {};
 (function(myself){
-	
-	myself.changeLocation = function (newLocation, bookmarks, isNew){
-        if(!newLocation){ return; }
-        var hash = (bookmarks && !_.isEmpty(bookmarks) ) ? '#' + generateHashValue( "bookmark" , { impl: "client" , params: bookmarks } ) : "";
-        if (isNew){
-        	window.open( newLocation + hash);
-    		} else {
-    			window.location = newLocation + hash;
-    	}
+  
+  myself.changeLocation = function (newLocation, bookmarks, isNew){
+  if(!newLocation){ return; }
+    var hash = (bookmarks && !_.isEmpty(bookmarks) ) ? '#' + generateHashValue( "bookmark" , { impl: "client" , params: bookmarks } ) : "";
+    if (isNew){
+      window.open( newLocation + hash);
+      } else {
+        window.location = newLocation + hash;
     }
+  }
 
-  	function generateHashValue (key, value) {
-        var obj = Dashboards.getHashValue(),json;
-        if (arguments.length == 1) {
-            obj = key;
-        } else {
-            obj[key] = value;
+    function generateHashValue (key, value) {
+    var obj = Dashboards.getHashValue(),json;
+    if (arguments.length == 1) {
+      obj = key;
+    } else {
+      obj[key] = value;
+    }
+    json = JSON.stringify(obj);
+    return json;
+  }
+
+  myself.isValidName = function (name){
+    return !(name === "" || name.split(" ").length > 1 || typeof name === "undefined")
+  }  
+
+
+  myself.isJobError = function(json){
+    return !json || ( json.hasOwnProperty('result') && json.result === false );
+  }   
+
+  myself.createElementsTableEmptyRawData = function() {
+    var emptyData = {
+      metadata:[
+        {
+          colIndex: 0,
+          colType: 'String',
+          colName: 'elementName'
+        },
+        {
+          colIndex: 1,
+          colType: 'String',
+          colName: 'elementType'
+        },
+        {
+          colIndex: 2,
+          colType: 'Boolean',
+          colName: 'adminOnly'
+        },
+        {
+          colIndex: 3,
+          colType: 'String',
+          colName: 'fileName'
+        },
+        {
+          colIndex: 4,
+          colType: 'String',
+          colName: 'elementName'
         }
-        json = JSON.stringify(obj);
-        return json;
+      ],
+      queryInfo:{
+        totalRows: 0
+      },
+      resultset: []
+    }
+    return emptyData;
+  }  
+
+
+myself.addUploadForm = function(ph, opts){
+    var _opts = {
+      root:'.',
+      success: function (filename) {
+        alert('File uploaded');
+      },
+      uploadError: function () {
+        alert('Error uploading file');
+      },
+      validationError: function () {
+        alert('File upload not permited.');
+      }
     }
 
-    myself.isValidName = function (name){
-        return !(name === "" || name.split(" ").length > 1 || typeof name === "undefined")
-    }    
+    opts = _.extend({}, _opts, opts);
+
+    var $ph = $(ph)
+        myself = this,
+        $uploadForm = $('<form action="../cfr/store" method="post" enctype="multipart/form-data">').addClass('WDhidden'),
+        filename = '';
+
+    var resetUploadForm = function () {
+      $fileInput.val('');
+      filename = '';
+    };
+
+    var fileUploaded = function (response) {
+      if (response.result) {
+        opts.success(filename);
+        resetUploadForm(filename);
+      } else {
+        opts.uploadError(filename);
+      }
+    };
+
+    // configure file upload form
+    $uploadForm.ajaxForm({
+      dataType: 'json',
+      success: fileUploaded,
+      error: opts.uploadError 
+    });
+
+    $ph.append($uploadForm);
+
+    var $fileInput = $('<input type="file" class="file" name="file" required/>').appendTo($uploadForm),
+        $pathInput = $('<input type="hidden" name="path" value="' + opts.root + '"/>').appendTo($uploadForm),
+        $submitInput = $('<button type="submit">').appendTo($uploadForm);
+
+    $fileInput.attr("id", this.htmlObject + "_file");
+    $fileInput.change(function () {
+      if ($fileInput.val() !== "") {
+        filename = $fileInput.val();
+        if (filename.slice(3, 11) == "fakepath") {
+          filename = filename.slice(12, filename.length);
+        }
+        $submitInput.click();
+      }
+    });
+
+    return function (){
+      $fileInput.click();
+    }
+  }
 
 
-    myself.isJobError = function(json){
-        return !json || ( json.hasOwnProperty('result') && json.result === false );
-    }   
-
-    myself.createElementsTableEmptyRawData = function() {
-    	var emptyData = {
-    		metadata:[
-    			{
-    				colIndex: 0,
-    				colType: 'String',
-    				colName: 'elementName'
-    			},
-    			{
-    				colIndex: 1,
-    				colType: 'String',
-    				colName: 'elementType'
-    			},
-    			{
-    				colIndex: 2,
-    				colType: 'Boolean',
-    				colName: 'adminOnly'
-    			},
-    			{
-    				colIndex: 3,
-    				colType: 'String',
-    				colName: 'fileName'
-    			},
-    			{
-    				colIndex: 4,
-    				colType: 'String',
-    				colName: 'elementName'
-    			}
-    		],
-    		queryInfo:{
-    			totalRows: 0
-    		},
-    		resultset: []
-    	}
-    	return emptyData;
-    }	
+  myself.testFile = function (url, successCallback, errorCallback){
+    successCallback = successCallback || function (){ return true };
+    errorCallback = errorCallback || function(){ return true }
+    $.ajax({
+      url: url,
+      type:'HEAD',
+      error: errorCallback,
+      success: successCallback
+    });
+  }
 
 })(sparkl);
 
@@ -76,87 +153,87 @@ var sparkl = {};
 
 ;(function (){
 
-	var parameterButton = {
-	    name: "parameterButton",
-	    label: "Parameter Button",
-	    defaults: {
-	    	buttons:[
-		    	{
-			    	cssClass: "viewButton",
-			    	title: "View",
-			      	action: function(v, st) {
-			      		Dashboards.log(v);
-			      	}
-		    	}
-	    	]
-	    },
+  var parameterButton = {
+    name: "parameterButton",
+    label: "Parameter Button",
+    defaults: {
+      buttons:[
+        {
+          cssClass: "viewButton",
+          title: "View",
+            action: function(v, st) {
+              Dashboards.log(v);
+            }
+        }
+      ]
+    },
 
-	    init: function(){
-	      	$.fn.dataTableExt.oSort[this.name+'-asc'] = $.fn.dataTableExt.oSort['string-asc'];
-	      	$.fn.dataTableExt.oSort[this.name+'-desc'] = $.fn.dataTableExt.oSort['string-desc'];
-	    },
-	    
-	    implementation: function(tgt, st, opt){
-	    	var $buttonContainer = $('<div/>').addClass('buttonContainer');
-	    	_.each(opt.buttons, function(el,idx){
-		    	var $button = $("<button/>").addClass(el.cssClass||"").text(el.title||"");
-		    	$button.click(function(){
-		    		if (el.action) {
-		    			el.action(st.value, st);
-		    		}
-		    	});
-		    	$buttonContainer.append($button);
-	    	});
-	    	$(tgt).empty().append($buttonContainer);
+    init: function(){
+        $.fn.dataTableExt.oSort[this.name+'-asc'] = $.fn.dataTableExt.oSort['string-asc'];
+        $.fn.dataTableExt.oSort[this.name+'-desc'] = $.fn.dataTableExt.oSort['string-desc'];
+    },
+    
+    implementation: function(tgt, st, opt){
+      var $buttonContainer = $('<div/>').addClass('buttonContainer');
+      _.each(opt.buttons, function(el,idx){
+        var $button = $("<button/>").addClass(el.cssClass||"").text(el.title||"");
+        $button.click(function(){
+          if (el.action) {
+            el.action(st.value, st);
+          }
+        });
+        $buttonContainer.append($button);
+      });
+      $(tgt).empty().append($buttonContainer);
 
-	    }
+    }
 
-  	};
-  	Dashboards.registerAddIn("Table", "colType", new AddIn(parameterButton));
-	
-	
+    };
+    Dashboards.registerAddIn("Table", "colType", new AddIn(parameterButton));
+  
+  
 /* edit data of table  */
   var editable = {
-    name: "editable",
-    label: "Editable",
-    defaults: {
-		action: function (v, st) {
-			Dashboards.log(v);
-		}
-    },
-    init: function(){
-        
-      // Register this for datatables sort
-      var myself = this;
-      $.fn.dataTableExt.oSort[this.name+'-asc'] = function(a,b){
-        return myself.sort(a,b)
-      };
-      $.fn.dataTableExt.oSort[this.name+'-desc'] = function(a,b){
-        return myself.sort(b,a)
-      };     
-    }, 
-    sort: function(a,b){
-      return this.sumStrArray(a) - this.sumStrArray(b);
-    }, 
-
-    implementation: function (tgt, st, opt) {
-      var t = $(tgt);
-	  var value = st.value;
-	  var text = $("<input/>").attr({value:value, type:'text', class:'editBox'})
-		.keyup(function(event){
-			if (event.keyCode == 13) {
-				opt.action( $(this).val(), st );
-			}
-			/*var idx = this.parentNode.parentNode.rowIndex;
-			metadataParam[idx-1][1] = $(this).val();*/
-			var obj = this.parentNode.parentNode.children[0].textContent;
-			metadataParam[obj.toString()] = $(this).val();
-		});
-	  
-	  t.empty();
-	  t.append(text);
+  name: "editable",
+  label: "Editable",
+  defaults: {
+    action: function (v, st) {
+      Dashboards.log(v);
     }
+  },
+  init: function(){
+    
+    // Register this for datatables sort
+    var myself = this;
+    $.fn.dataTableExt.oSort[this.name+'-asc'] = function(a,b){
+    return myself.sort(a,b)
+    };
+    $.fn.dataTableExt.oSort[this.name+'-desc'] = function(a,b){
+    return myself.sort(b,a)
+    };   
+  }, 
+  sort: function(a,b){
+    return this.sumStrArray(a) - this.sumStrArray(b);
+  }, 
+
+  implementation: function (tgt, st, opt) {
+    var t = $(tgt);
+    var value = st.value;
+    var text = $("<input/>").attr({value:value, type:'text', class:'editBox'})
+    .keyup(function(event){
+      if (event.keyCode == 13) {
+        opt.action( $(this).val(), st );
+      }
+      /*var idx = this.parentNode.parentNode.rowIndex;
+      metadataParam[idx-1][1] = $(this).val();*/
+      var obj = this.parentNode.parentNode.children[0].textContent;
+      metadataParam[obj.toString()] = $(this).val();
+    });
+    
+    t.empty();
+    t.append(text);
+  }
   };
   Dashboards.registerAddIn("Table", "colType", new AddIn(editable));
-	
+  
 })();
